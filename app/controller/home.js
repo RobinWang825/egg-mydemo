dcf0d2bacb5a0ce0b764c6c48efbad7ab7c50ebd
wangshi <531182778@qq.com>
2021-11-13 19:29:21 +0800

    添加登录校验v1 -- 实现简单的验证用户名和密码

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,19 +3,48 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  // http://localhost:7001/
-  async index() {
-    const { ctx } = this;
-    const params = ctx.query;
-    // if(params.name='刘学炜'){
-    //   ctx.body = {
-    //     success: false,
-    //     message: '没有权限',
-    //     code: 403
-    //   };
-    // }
-    ctx.body = `<p>hi, ${params.name || 'egg'}</p>`;
+  // 添加登录校验v1 -- 实现简单的验证用户名和密码
+  async index() { // login表单界面
+    this.ctx.body = `
+      <form action = "/login">
+        <label>登录名</label><input name="username"><br/>
+        <label>密码</label><input type="password" name="password">
+        <br/>
+        <button>提交</button>
+      </form>
+      `;
   }
+  async login(){ //login逻辑 校验
+    const { ctx } = this;
+    const username = ctx.query.username;
+    const password = ctx.query.password;
+    if (username === "admin" && password === "123456") {
+      // ctx.body = username.toString()+"<h1>登陆成功</h1>";
+      ctx.body = "<h1>登陆成功</h1>";
+    }else{
+      ctx.body = "<h1>登陆失败</h1>";
+    }
+
+  }
+
+  // http://localhost:7001/
+  // async index() {
+  //   const { ctx } = this;
+  //   const params = ctx.query;
+  //   // if(params.name='刘学炜'){
+  //   //   ctx.body = {
+  //   //     success: false,
+  //   //     message: '没有权限',
+  //   //     code: 403
+  //   //   };
+  //   // }
+  //   ctx.body = `<p>hi, ${params.name || 'egg'}</p>`;
+
+
+
+
+
+  // }
   // http://localhost:7001/say-hello.json?name=马跃
   // http://localhost:7001/say-hello.json?name=刘学炜
   async sayHello() {
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/login',controller.home.login);
   router.get('/say-hello.json', controller.home.sayHello);
   router.get('/index.htm', controller.home.home);
   router.get('/send-email', controller.email.sendEmail);
